db61faf179d58db4c4b3e57b7bcd6049027474a0
daijita <976185561@qq.com>
2022-12-13 15:18:19 +0800

    add local 135-137 code

--- solution/demo136.py
+++ solution/demo136.py
@@ -1,19 +1,11 @@
-# encoding=utf8
-import sys
 
 
-def yihuo(a, b):
-    if a == b:
-        return 0
-    else:
-        return
+def singe_number(arr):
 
-
-def demo137(arr):
     if len(arr) == 0:
         return None
 
-    if len(arr) <= 3:
+    if len(arr) <= 2:
         return None
 
     lst = [0] * 32
@@ -24,11 +16,10 @@ def demo137(arr):
             index = 31 - j
             lst[index] += int(e)
 
-    lst = [str(i % 3) for i in lst]
+    lst = [str(i % 2) for i in lst]
     return int(''.join(lst), 2)
 
-
 if __name__ == '__main__':
-    arr = [2, 2, 2, 9, 6, 9, 6, 9, 56, 6]
-    res = demo137(arr)
+    arr = [2, 2, 9, 9, 3, 3]
+    res = singe_number(arr)
     print(res)

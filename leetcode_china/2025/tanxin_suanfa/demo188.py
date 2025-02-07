2beb44087a1e369dbe4d5d572e01fe2c110bbbed
daijitao <976185561@qq.com>
2025-02-07 23:00:42 +0800

    add

---
+++ leetcode_china/2025/tanxin_suanfa/demo188.py
@@ -0,0 +1,6 @@
+from typing import List, Optional
+
+# https://leetcode.cn/problems/best-time-to-buy-and-sell-stock-iv/
+
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
---
+++ leetcode_china/2025/tanxin_suanfa/demo455.py
@@ -0,0 +1,8 @@
+from typing import List, Optional
+
+
+"""
+https://leetcode.cn/problems/assign-cookies/description/
+"""
+def demo():
+    pass
